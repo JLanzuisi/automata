b64227afb572ea8bebb19453406b82e26149d255
Jhonny Lanzuisi <jalb97@gmail.com>
2024-02-03 16:52:28 -0400

    Remove go implementation

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-#goimports -w . && staticcheck && go build -o bin/conway
-
 cc -Wall -Wextra -pedantic -o bin/cgol cgol.c gifenc.c
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/JLanzuisi/conway
-
-go 1.21.6
